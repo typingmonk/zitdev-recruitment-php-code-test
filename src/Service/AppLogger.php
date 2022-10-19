254672c623c4015298f14d5e555608616801e104
tmonk <tmonk.hsieh@tmonk.tw>
2022-10-19 14:12:23 +0800

    題目 3

--- src/Service/AppLogger.php
+++ src/Service/AppLogger.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Service;
+use think\LogManager;
 
 class AppLogger
 {
-    const TYPE_LOG4PHP = 'log4php';
+	const TYPE_LOG4PHP = 'log4php';
+	const TYPE_THINK_LOG = 'think-log';
 
     private $logger;
 
@@ -12,21 +14,35 @@ class AppLogger
     {
         if ($type == self::TYPE_LOG4PHP) {
             $this->logger = \Logger::getLogger("Log");
-        }
+		} else if ($type = self::TYPE_THINK_LOG) {
+			$this->logger = new LogManager();
+		}
     }
 
     public function info($message = '')
-    {
-        $this->logger->info($message);
+	{
+		if($this->logger instanceof LogManager){
+			$this->logger->info(strtoupper($message));
+		} else {
+			$this->logger->info($message);
+		}
     }
 
     public function debug($message = '')
     {
-        $this->logger->debug($message);
+		if($this->logger instanceof LogManager){
+			$this->logger->debug(strtoupper($message));
+		} else {
+        	$this->logger->debug($message);
+		}
     }
 
     public function error($message = '')
     {
-        $this->logger->error($message);
+		if($this->logger instanceof LogManager){
+			$this->logger->error(strtoupper($message));
+		} else {
+        	$this->logger->error($message);
+		}
     }
 }
