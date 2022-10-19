39217d672037de13dea3264f93f857159f1611d7
tmonk <tmonk.hsieh@tmonk.tw>
2022-10-19 14:11:20 +0800

    題目 2

--- tests/App/DemoTest.php
+++ tests/App/DemoTest.php
@@ -9,16 +9,21 @@
 
 namespace Test\App;
 
+use App\App\Demo;
+use App\Util\HttpRequest;
 use PHPUnit\Framework\TestCase;
-
 
 class DemoTest extends TestCase {
 
     public function test_foo() {
-
+		$demo = new Demo("", new HttpRequest());
+		$value = $demo->foo();
+		$this->assertEquals("bar", $value);	
     }
 
     public function test_get_user_info() {
-
+		$demo = new Demo("", new HttpRequest());
+		$value = $demo->get_user_info();
+		$this->assertEquals(null, $value);
     }
 }
