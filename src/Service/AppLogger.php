50b7a48bc5456ab17a23c38a904083ecb1e49c3c
tmonk <tmonk.hsieh@tmonk.tw>
2022-10-24 16:13:30 +0800

    使用策略模式實作題目 3

--- src/Service/AppLogger.php
+++ src/Service/AppLogger.php
@@ -3,46 +3,71 @@
 namespace App\Service;
 use think\LogManager;
 
+interface LoggerStrategy
+{
+	public function info($msg);
+	public function debug($msg);
+    public function error($msg);	
+}
+
+class Log4phpStrategy implements LoggerStrategy
+{
+	private $logger;
+
+	public function __construct(){
+		$this->logger = \Logger::getLogger("Log");
+	}
+
+	public function info($msg = ''){
+		$this->logger->getLogger("Log")->info($msg);
+	}
+
+	public function debug($msg){
+		$this->logger->debug($msg);
+	}
+
+	public function error($msg){
+		$this->logger->error($msg);
+	}
+}
+
+class ThinklogStrategy implements LoggerStrategy
+{
+	private $logger;
+
+	public function __construct(){
+		$this->logger = new LogManager();
+    }
+
+	public function info($msg = ''){
+		$this->logger->info(strtoupper($msg));
+	}
+
+	public function debug($msg){
+		$this->logger->debug(strtoupper($msg));
+	}
+
+	public function error($msg){
+		$this->logger->error(strtoupper($msg));
+	}
+}
+
 class AppLogger
 {
 	const TYPE_LOG4PHP = 'log4php';
 	const TYPE_THINK_LOG = 'think-log';
 
-    private $logger;
-
-    public function __construct($type = self::TYPE_LOG4PHP)
-    {
-        if ($type == self::TYPE_LOG4PHP) {
-            $this->logger = \Logger::getLogger("Log");
-		} else if ($type = self::TYPE_THINK_LOG) {
-			$this->logger = new LogManager();
+	private $strategy = NULL;
+	public function __construct($loggerStrategy = self::TYPE_LOG4PHP) {
+		switch ($loggerStrategy) {
+			case self::TYPE_LOG4PHP:
+				$this->strategy = new Log4phpStrategy();
+				break;
+			case self::TYPE_THINK_LOG:
+				$this->strategy = new ThinklogStrategy();
+				break;	
+			default:
+				$this->strategy = new Log4phpStrategy();
 		}
-    }
-
-    public function info($message = '')
-	{
-		if($this->logger instanceof LogManager){
-			$this->logger->info(strtoupper($message));
-		} else {
-			$this->logger->info($message);
-		}
-    }
-
-    public function debug($message = '')
-    {
-		if($this->logger instanceof LogManager){
-			$this->logger->debug(strtoupper($message));
-		} else {
-        	$this->logger->debug($message);
-		}
-    }
-
-    public function error($message = '')
-    {
-		if($this->logger instanceof LogManager){
-			$this->logger->error(strtoupper($message));
-		} else {
-        	$this->logger->error($message);
-		}
-    }
+	}
 }
